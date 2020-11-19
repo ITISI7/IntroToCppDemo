3aacbd743f127c60ca6547b3395249270becdbad
David <7319dtounn@gmail.com>
2020-11-19 11:33:07 -0800

    new

---
+++ IntroToCppDemo/binary/Main.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+int binary_search(int array[], int start_index, int end_index, int key)
+{
+	return 0;
+}
+int main()
+{
+	int number[] = { 0,2,4,6,8 };
+	int searchidx = binary_search(number, 0, 4, 6);
+	std::cout << searchidx;
+	return 0;
+}
---
+++ IntroToCppDemo/ticTacTo/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+
+int borad[3][3] = { 1,2,3,4,5,6,7,8,9 };
+int placement = 0;
+
+int main()
+{
+	if (placement == 0)
+	{
+		cout << " ";
+	}
+	else if (placement == 1)
+	{
+		cout << "X";
+	}
+	
+}
